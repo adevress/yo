a4b191f1ad3529f2161cbd114bd2852b760791ac
Adrien Devresse <adrien.devresse@epfl.ch>
2018-06-14 15:09:50 +0200

    First valid python implementation

---
+++ python/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+###
+##    yo, parallel I/O utilities
+##    Copyright (C) 2018 Adrien Devresse
+##
+##  This Source Code Form is subject to the terms of the Mozilla Public
+##  License, v. 2.0. If a copy of the MPL was not distributed with this
+##  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+##
+
+
+from . _yo import (copyfile)
